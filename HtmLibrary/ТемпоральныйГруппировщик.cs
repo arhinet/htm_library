916d6f50bb8a2f905e47bd57c60335e2b4328e22
Alex Vrubel <avrubel@boardmaps.com>
2013-07-05 18:42:41 +0400

    Перенесена Имплементация пространственного группировщика из книги

---
+++ HtmLibrary/ТемпоральныйГруппировщик.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HtmLibrary
+{
+    public class ТемпоральныйГруппировщик
+    {
+        /// <summary>
+        /// Фаза 1: вычисление активных состояний activeState(t) для каждой клетки.
+        /// </summary>
+        public void ВычислениеАктивныхСостояний()
+        {
+            
+        }
+
+        
+        /// <summary>
+        /// Фаза 2: вычисление состояний предчувствия (предсказания) predictiveState(t) для каждой клетки.
+        /// </summary>
+        public void ВычислениеСостоянийПредчувствия()
+        {
+            
+        }
+
+
+        /// <summary>
+        /// Фаза 3: обновление синапсов.
+        /// </summary>
+        /// <remarks>
+        /// необходима только для обучения.
+        /// </remarks>
+        public void ОбновлениеСинапсов()
+        {
+            
+        }
+        
+        
+
+    }
+}
